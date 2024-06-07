7ee04d600d2a6763c0b2d686de53c0c549ffd408
Sahil Koirala <koirala24sahil@gmail.com>
2024-06-07 09:02:09 +0545

    Conditional Statement

--- lib/mainScreen.dart
+++ lib/mainScreen.dart
@@ -9,11 +9,31 @@ class Mainscreen extends StatefulWidget {
 }
 
 class _MainscreenState extends State<Mainscreen> {
+  List<dynamic> bucketListData = [];
+  bool isLoading = false;
   Future<void> getData() async {
+    setState(() {
+      isLoading = true;
+    });
     //get data from the API
-    var response = await Dio().get(
-        "https://flutterapitest123-d1627-default-rtdb.firebaseio.com/bucketlist.json");
-    print(response);
+    try {
+      Response response = await Dio().get(
+          "https://flutterapitest123-d1627-default-rtdb.firebaseio.com/bucketlist.json");
+
+      bucketListData = response.data;
+      isLoading = false;
+      setState(() {});
+    } catch (e) {
+      isLoading = false;
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    getData();
+    super.initState();
   }
 
   @override
@@ -22,8 +42,38 @@ class _MainscreenState extends State<Mainscreen> {
       appBar: AppBar(
         centerTitle: true,
         title: Text("Bucket List"),
+        actions: [
+          InkWell(
+              onTap: getData,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(Icons.refresh_rounded),
+              ))
+        ],
       ),
-      body: ElevatedButton(onPressed: getData, child: Text("Get Data")),
+      body: Expanded(
+          child: RefreshIndicator(
+        onRefresh: getData,
+        child: isLoading
+            ? Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                itemCount: bucketListData.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      title: Text(bucketListData[index]["item"] ?? ""),
+                      leading: CircleAvatar(
+                        backgroundImage:
+                            NetworkImage(bucketListData[index]["image"] ?? ""),
+                      ),
+                      trailing:
+                          Text(bucketListData[index]["cost"].toString() ?? ""),
+                    ),
+                  );
+                },
+              ),
+      )),
     );
   }
 }
